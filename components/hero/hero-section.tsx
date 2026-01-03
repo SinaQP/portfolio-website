51fa8b604918ddd1fe7c7d43c692578dc0adaebf
SinaSQP <qsempoursina@gmail.com>
2026-01-03 18:46:37 +0330

    Add download functionality to Resume button in HeroSection component

--- components/hero/hero-section.tsx
+++ components/hero/hero-section.tsx
@@ -94,7 +94,19 @@ export function HeroSection() {
                 <Mail className="w-5 h-5 mr-2 inline-block group-hover:scale-110 transition-transform" />
                 Contact
               </NeonButton>
-              <NeonButton variant="lime" size="lg" className="group">
+              <NeonButton
+                variant="lime"
+                size="lg"
+                className="group"
+                onClick={() => {
+                  const link = document.createElement("a")
+                  link.href = "/Sina-Qasempour-Resume.pdf"
+                  link.download = "Sina-Qasempour-Resume.pdf"
+                  document.body.appendChild(link)
+                  link.click()
+                  link.remove()
+                }}
+              >
                 <Download className="w-5 h-5 mr-2 inline-block group-hover:translate-y-1 transition-transform" />
                 Resume
               </NeonButton>
